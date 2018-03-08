06f74e9f9dd4fea1530aea4cbf8cf3018b7c727c
Nimay Kumar <kumar.nimay@yahoo.com>
2018-03-08 14:58:26 -0600

    added headers

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -1,16 +1,17 @@
 package assignment4;
-/* CRITTERS Critter.java
+/* CRITTERS Main.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Nimay Kumar
+ * nrk472
+ * 15470
+ * Benson Huang
+ * bkh642
+ * 15470
  * Slip days used: <0>
- * Fall 2016
+ * Spring 2018
  */
+
 
 
 import java.util.List;
