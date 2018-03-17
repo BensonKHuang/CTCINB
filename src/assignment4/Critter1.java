9e9de1f5749922db7bdc250bbb67285fed957290
Nimay Kumar <kumar.nimay@yahoo.com>
2018-03-16 21:03:55 -0500

    added stats for virus and type A/B

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -9,11 +9,12 @@ package assignment4;
 public class Critter1 extends Critter {
 
     private int dir;
-    private static int max_litter_size = 2;
+    private int type;
 
-    public Critter1(){
+    public Critter1() {
 
         dir = getRandomInt(8);
+        type = getRandomInt(2);
     }
 
     /**
@@ -25,15 +26,19 @@ public class Critter1 extends Critter {
 
 
         int new_dir = getRandomInt(8);
+
         walk(dir);
 
-        //int litter_size = getRandomInt(max_litter_size) + 1;
 
         if(getEnergy() >= 80){
+
             createOffspring();
+            if(type == 1){
+                createOffspring();
+            }
         }
 
-        while(new_dir != dir){
+        while (new_dir != dir) {
             new_dir = getRandomInt(8);
         }
 
@@ -42,6 +47,7 @@ public class Critter1 extends Critter {
 
     /**
      * Critter1 always fights
+     *
      * @param irrelevant is irrelevant
      * @return true every time
      */
@@ -50,14 +56,43 @@ public class Critter1 extends Critter {
         return true;
     }
 
-    private void createOffspring(){
+    public int getType(){
+        return type;
+    }
+
+    private void createOffspring() {
 
         Critter1 child = new Critter1();
         reproduce(child, getRandomInt(8));
     }
 
-    public String toString(){
+    public String toString() {
 
         return "V";
     }
+
+    public static void runStats(java.util.List<Critter> viruses){
+
+
+        int numA = 0;
+        int numB = 0;
+
+        for(Object o : viruses){
+
+            Critter1 c = (Critter1) o;
+            if(c.type == 0){
+                ++numA;
+            }
+            else{
+                ++numB;
+            }
+        }
+
+        System.out.println(viruses.size() + " total Viruses");
+        System.out.println(numA + " type A viruses");
+        System.out.println(numB + " type B viruses");
+    }
 }
+
+
+
